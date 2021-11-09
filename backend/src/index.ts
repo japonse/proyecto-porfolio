c439579a3d15af09c93f6df262c12243836a8c5d
japonse <javi.btk@gmail.com>
2021-11-09 16:08:58 +0100

    Added database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import { createConnection } from "typeorm"; 
 import masterRouter from './routes/index'; 
 
 const app = express();
 
 
-// routes
-app.use('/', masterRouter);
-app.get('*', function (req, res) { res.redirect('/error/notfound') });
+// connect to the database 
+createConnection().then((connection) => {
+    if (connection === undefined) {
+        app.get('*', function (req, res) { res.redirect('/error/dbconnection') }); //default route when DB connection fails
+        throw new Error('Error connecting to database');
+    } else { 
+        app.use((req, res, next) => { //make CORS not complain
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+            res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+            next();
+        });
+        app.use('/', masterRouter); //delegate route management to masterRoute
+        app.get('*', function (req, res) { res.redirect('/error/notfound') }); //default route
+    }
+});
+
 
 
 // start the Express server
--- backend/src/routes/error/error_routes.ts
+++ backend/src/routes/error/error_routes.ts
@@ -6,4 +6,8 @@ router.get('/notfound', (req, res) => {
     res.send("<html><h1>NOTFOUND 404</h1></html>");
 });
 
+router.get('/dbconnection', (req, res) => {
+    res.send("<html><h1>Unable to connect to the database</h1></html>");
+});
+
 export = router;
