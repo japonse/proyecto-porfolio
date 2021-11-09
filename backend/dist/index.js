46df4d0949fe39564e2920da1ec5adbf1f4e9823
japonse <javi.btk@gmail.com>
2021-11-09 17:41:40 +0100

    Consol log to see env variables

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -21,7 +21,16 @@ app.use((req, res, next) => {
 app.use('/', index_1.default); //delegate route management to masterRoute
 // connect to the database 
 (0, typeorm_1.createConnection)().then((connection) => {
+    var _a, _b, _c, _d, _e, _f, _g, _h;
     console.log('TRIED TO CONNECT');
+    console.log((_a = process.env.PORT) !== null && _a !== void 0 ? _a : 'nohay');
+    console.log((_b = process.env.DB_PORT) !== null && _b !== void 0 ? _b : 'nohay');
+    console.log((_c = process.env.DB_HOST) !== null && _c !== void 0 ? _c : 'nohay');
+    console.log((_d = process.env.DB_DATABASE) !== null && _d !== void 0 ? _d : 'nohay');
+    console.log((_e = process.env.DB_PASSWORD) !== null && _e !== void 0 ? _e : 'nohay');
+    console.log((_f = process.env.DB_USERNAME) !== null && _f !== void 0 ? _f : 'nohay');
+    console.log((_g = process.env.ENTITIES_PATH) !== null && _g !== void 0 ? _g : 'nohay');
+    console.log((_h = process.env.MIGRATIONS_PATH) !== null && _h !== void 0 ? _h : 'nohay');
     if (connection === undefined) {
         app.get('*', function (req, res) { res.redirect('/error/dbconnection'); }); //default route when DB connection fails
         console.log('BAD CONNECT 1');
