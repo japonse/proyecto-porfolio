b438f87ca92d2c7f94f0c7d31477d073ce5de679
japonse <javi.btk@gmail.com>
2021-11-09 16:01:56 +0100

    Added api controller & refactor routes

--- backend/src/routes/api/api_routes.ts
+++ backend/src/routes/api/api_routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import {createConnection} from "typeorm";
+import apiController from "../../controllers/api_controller";
 
 let router: express.Router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("<html><h1>I'm an API!</h1></html>");
+router.get('/',  (req: express.Request, res: express.Response) => {
+    res.send("<html><h1>I'm an API!</h1></html>")
 });
+
+router.get('/call', apiController.callControler);
+router.get('/recall', apiController.recallControler);
 
 export = router;
