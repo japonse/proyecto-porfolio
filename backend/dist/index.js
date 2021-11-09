120aa4494e6040dbf7dcd1acfe9d335c1c5177ab
japonse <javi.btk@gmail.com>
2021-11-09 19:29:27 +0100

    Changes on nodemon-tsc & Added packge concurrently

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a, _b, _c, _d, _e, _f, _g, _h;
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
@@ -10,7 +9,6 @@ const express_1 = __importDefault(require("express"));
 const typeorm_1 = require("typeorm");
 const index_1 = __importDefault(require("./routes/index"));
 const app = (0, express_1.default)();
-console.log('STARTED');
 // routes
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -21,31 +19,20 @@ app.use((req, res, next) => {
 });
 app.use('/', index_1.default); //delegate route management to masterRoute
 // connect to the database 
-console.log('TRIED TO CONNECT');
-console.log((_a = process.env.PORT) !== null && _a !== void 0 ? _a : 'nohay');
-console.log((_b = process.env.DB_PORT) !== null && _b !== void 0 ? _b : 'nohay');
-console.log((_c = process.env.DB_HOST) !== null && _c !== void 0 ? _c : 'nohay');
-console.log((_d = process.env.DB_DATABASE) !== null && _d !== void 0 ? _d : 'nohay');
-console.log((_e = process.env.DB_PASSWORD) !== null && _e !== void 0 ? _e : 'nohay');
-console.log((_f = process.env.DB_USERNAME) !== null && _f !== void 0 ? _f : 'nohay');
-console.log((_g = process.env.ENTITIES_PATH) !== null && _g !== void 0 ? _g : 'nohay');
-console.log((_h = process.env.MIGRATIONS_PATH) !== null && _h !== void 0 ? _h : 'nohay');
 (0, typeorm_1.createConnection)().then((connection) => {
     if (connection === undefined) {
         app.get('*', function (req, res) { res.redirect('/error/dbconnection'); }); //default route when DB connection fails
-        console.log('BAD CONNECT 1');
+        throw new Error('Error connecting to database');
     }
     else {
-        console.log('CONNECT SUCESS');
         app.get('*', function (req, res) { res.redirect('/error/notfound'); }); //default route
     }
 }).catch((error) => {
-    console.log('BAD CONNECT 2');
-    console.log('error', error);
     app.get('*', function (req, res) { res.redirect('/error/dbconnection'); }); //default route when DB connection fails
+    throw new Error('Error connecting to database');
 });
 // start the Express server
 app.listen(process.env.PORT, () => {
-    console.log(`server started at http://localhost:${process.env.PORT}`);
+    console.log(`server started at port: ${process.env.PORT}`);
 });
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,6 @@ import { createConnection } from "typeorm";
 import masterRouter from './routes/index'; 
 
 const app = express();
-console.log('STARTED');
 
 // routes
 app.use((req, res, next) => { //make CORS not complain
@@ -19,34 +18,19 @@ app.use((req, res, next) => { //make CORS not complain
 app.use('/', masterRouter); //delegate route management to masterRoute
 
 // connect to the database 
-console.log('TRIED TO CONNECT');
-console.log(process.env.PORT ?? 'nohay');
-console.log(process.env.DB_PORT ?? 'nohay');
-console.log(process.env.DB_HOST ?? 'nohay');
-console.log(process.env.DB_DATABASE ?? 'nohay');
-console.log(process.env.DB_PASSWORD ?? 'nohay');
-console.log(process.env.DB_USERNAME ?? 'nohay');
-console.log(process.env.ENTITIES_PATH ?? 'nohay');
-console.log(process.env.MIGRATIONS_PATH ?? 'nohay');
-
 createConnection().then((connection) => {
     if (connection === undefined) {
         app.get('*', function (req, res) { res.redirect('/error/dbconnection') }); //default route when DB connection fails
-        console.log('BAD CONNECT 1');
+        throw new Error('Error connecting to database');
     } else { 
-        console.log('CONNECT SUCESS');
         app.get('*', function (req, res) { res.redirect('/error/notfound') }); //default route
     }
 }).catch((error)=>{
-    console.log('BAD CONNECT 2');
-
-    console.log('error',error);
-
     app.get('*', function (req, res) { res.redirect('/error/dbconnection') }); //default route when DB connection fails
+    throw new Error('Error connecting to database');
 });
-
 
 // start the Express server
 app.listen(process.env.PORT, () => {
-    console.log(`server started at http://localhost:${process.env.PORT}`);
+    console.log(`server started at port: ${process.env.PORT}`);
 });
