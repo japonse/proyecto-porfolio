52ab7731b45b7176b4e5ad076c6f34feddf8ac51
japonse <javi.btk@gmail.com>
2021-11-17 21:59:31 +0100

    Some dev test

---
+++ frontend/src/app/components/main/main.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit, Input} from '@angular/core';
+import {ElementRef} from '@angular/core';
+import {lightTheme} from 'src/app/themes/light-theme';
+import {darkTheme} from 'src/app/themes/dark-theme';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  @Input() headerConsoleComand?: string = "";
+
+  headerConsoleRoute:string         = "";
+  headerConsoleDesiredComand:string = "cd Ampersand";
+  currentTheme                      = darkTheme;
+
+  urlImg: string  = "https://avatars.githubusercontent.com/u/67862213?s=400&amp;u=df6f061706ddf5427b28fa2d5cd031932e626238&amp;v=4";
+  urlImg2: string = "https://www.solucionex.com/sites/default/files/posts/imagen/css_blog.png"
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+    this.typeLetter();
+  }
+
+  typeLetter(): void{
+    var headerConsoleComandLength:number = (this.headerConsoleComand) ? this.headerConsoleComand.length : 0;
+    this.headerConsoleComand = this.headerConsoleDesiredComand.substr(0, headerConsoleComandLength +1);
+    if(this.headerConsoleComand.length < this.headerConsoleDesiredComand.length){
+      setTimeout(()=>{this.typeLetter()},100);
+    }else{
+      setTimeout(()=>{
+        this.headerConsoleRoute  = "Ampersand";
+        this.headerConsoleComand = "_";
+      },500);
+    }
+  }
+
+  getUsers(): void{
+
+  }
+
+  changeTheme():void{
+    this.currentTheme = (this.currentTheme.name  == 'dark') ? lightTheme : darkTheme;
+
+    for (const property in this.currentTheme.properties) {
+      this.elementRef.nativeElement.closest('body').style.setProperty(property,this.currentTheme.properties[property]);
+    }
+  }
+
+  execAmpersandShell(){
+    switch(this.headerConsoleComand && this.headerConsoleComand.toLowerCase() || ""){
+      case 'theme light':
+        this.currentTheme = darkTheme;
+        this.changeTheme();
+        this.headerConsoleComand = "💡";
+      break;
+      case 'theme dark':
+        this.currentTheme = lightTheme;
+        this.changeTheme();
+        this.headerConsoleComand = "🦇";
+      break;
+      case 'ampersand_':
+        this.headerConsoleComand = "help";
+      break;
+      case 'help':
+        this.headerConsoleComand = "Type 'theme dark' or 'theme light' to change between themes."
+      break;
+      case '':
+        this.headerConsoleComand = "Type 'theme dark' or 'theme light' to change between themes."
+      break;
+      case 'command not found':
+        this.headerConsoleComand = ""
+      break;
+      default:
+        this.headerConsoleComand = "Command not found";
+      break;
+    }
+  }
+
+}
---
+++ frontend/src/app/themes/light-theme.ts
@@ -0,0 +1,14 @@
+export const lightTheme: {name:string, properties: { [key: string]: string }} = {
+  name: 'light',
+  properties:{
+    "--color_bg_header": "rgb(5, 9, 16)",
+    "--color_bg": "rgb(238,238,238)",
+    "--color_bg_card": "rgb(255,255,255)",
+    "--color_bg_card_title": "rgb(255,255,255)",
+    "--color_font_card": "rgb(43,50,57)",
+    "--color_text_shadow": "rgba(255,255,255,0)",
+    "--color_card_title_border": "rgba(241,241,241,1)",
+    "--bg_shadow_top": "0px 0px 2200px 150px rgba(102, 101, 119, 1)",
+    "--bg_shadow_bottom": "0px 0px 2200px 150px rgba(102, 101, 119, 0.5)"
+  }
+};
