8b7186c1ebeaf3942231d5be733a4e5d02ca522a
japonse <javi.btk@gmail.com>
2022-05-05 12:28:49 +0200

    Change names of controller functions

--- backend/dist/controllers/api_controller.js
+++ backend/dist/controllers/api_controller.js
@@ -1,10 +1,11 @@
 "use strict";
 const apiController = {
-    callControler: (req, res, next) => {
+    call: (req, res, next) => {
         req.session.views = (req.session.views || 0) + 1;
-        res.send("<html><h1>Called! " + req.session.views + "</h1></html>");
+        res.send({ hola: "adios" });
+        //res.send("<html><h1>Called!!! "+req.session.views+"</h1></html>");
     },
-    recallControler: (req, res, next) => {
+    recall: (req, res, next) => {
         res.send("<html><h1>Recalled!</h1></html>");
     },
 };
--- backend/src/routes/api/api_routes.ts
+++ backend/src/routes/api/api_routes.ts
@@ -7,8 +7,8 @@ router.get('/',  (req: express.Request, res: express.Response) => {
     res.send("<html><h1>I'm an API!</h1></html>")
 });
 
-router.get('/call', apiController.callControler);
-router.get('/recall', apiController.recallControler);
+router.get('/call', apiController.call);
+router.get('/recall', apiController.recall);
 router.get('*', function (req, res) { res.redirect('/error/notfound') });
 
 export = router;
